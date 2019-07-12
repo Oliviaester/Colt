a4b42b40670fe39212a2a30d6e8796528a7ae224
Olivia Ester <oliviaesterdev@gmail.com>
2019-07-12 13:40:38 -0700

    practice

--- friends/src/Friend.js
+++ friends/src/Friend.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './app.css';
 
 class Friend extends React.Component {
   render() {
     const { name, hobbies } = this.props;
     return (
-      <div>
+      <div className='Friend'>
         <h1>{name}</h1>
         <ul>
           {hobbies.map(h => (
