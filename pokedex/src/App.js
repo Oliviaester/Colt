570ec5863379d036806669ebf085003ea59cedb9
Olivia Ester <oliviaesterdev@gmail.com>
2019-07-12 17:34:03 -0700

    practice

---
+++ pokedex/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Pokegame from './Pokegame';
+import './Pokecard.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Pokegame />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ pokedex/src/Pokedex.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import Pokecard from './Pokecard';
+import './Pokedex.css';
+
+class Pokedex extends React.Component {
+  render() {
+    let title;
+    if (this.props.isWinner) {
+      title = <h1 className='Pokedex-winner'>Winning Hand</h1>;
+    } else {
+      title = <h1 className='Pokedex-loser'>Losing Hand</h1>;
+    }
+    return (
+      <div className='Pokedex'>
+        {title}
+        <h4>Total exh4erience: {this.props.exp}</h4>
+
+        <div className='Pokedex-card'>
+          {this.props.pokemon.map(p => (
+            <div key={p.id}>
+              <Pokecard
+                id={p.id}
+                name={p.name}
+                type={p.type}
+                exp={p.base_experience}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Pokedex;
