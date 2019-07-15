e66a7d94a49e99a5df568a3057412d70efb99a33
Olivia Ester <oliviaesterdev@gmail.com>
2019-07-15 12:46:36 -0700

    practice

---
+++ clicker/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Clicker from './Clicker';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Clicker />
+    </div>
+  );
+}
+
+export default App;
