687e9e4d7abd5cc0750b33c33a7890d0c61b900e
Olivia Ester <oliviaesterdev@gmail.com>
2019-07-16 15:43:10 -0700

    practice

--- dice/src/RollDice.js
+++ dice/src/RollDice.js
@@ -7,13 +7,30 @@ class RollDice extends Component {
   };
   constructor(props) {
     super(props);
-    this.state = { die1: 'one', die2: 'one' };
+    this.state = { die1: 'one', die2: 'one', rolling: false };
+    this.roll = this.roll.bind(this);
+  }
+  roll() {
+    const newDie1 = this.props.sides[
+      Math.floor(Math.random() * this.props.sides.length)
+    ];
+    const newDie2 = this.props.sides[
+      Math.floor(Math.random() * this.props.sides.length)
+    ];
+    this.setState({ die1: newDie1, die2: newDie2, rolling: true });
+
+    setTimeout(() => {
+      this.setState({ rolling: false });
+    }, 1000);
   }
   render() {
     return (
       <div>
         <Die face={this.state.die1} />
         <Die face={this.state.die2} />
+        <button onClick={this.roll} disabled={this.state.rolling}>
+          {this.state.rolling ? 'Rolling...' : 'Roll Dice'}
+        </button>
       </div>
     );
   }
