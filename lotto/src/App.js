7811c228acddeb9e6c67cb3f34098023df6acef7
Olivia Ester <oliviaesterdev@gmail.com>
2019-07-17 14:22:40 -0700

    practice

---
+++ lotto/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Ball from './Ball';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <Ball num={17} />
+      <Ball num={17} />
+      <Ball num={17} />
+      <Ball num={17} />
+    </div>
+  );
+}
+
+export default App;
