64c2ae181b5c131450717c81441dbbb470127cfe
Olivia Ester <oliviaesterdev@gmail.com>
2019-08-02 14:43:19 -0700

    practice

--- cards/src/Deck.js
+++ cards/src/Deck.js
@@ -1,22 +1,55 @@
 import React, { Component } from 'react';
-import axios from 'axios'
-const API_URL = 'https://deckofcardsapi.com/api/deck/new/shuffle';
+import './Deck.css';
+import Card from './Card';
+import axios from 'axios';
+const API_BASE_URL = 'https://deckofcardsapi.com/api/deck';
 
 class Deck extends Component {
   constructor(props) {
     super(props);
-    this.state = { deck: null };
+    this.state = { deck: null, drawn: [] };
+    this.getCard = this.getCard.bind(this);
   }
 
-componentDidMount() {
-  let deck = await axios.get(API_URL)
-  this.setState({deck: deck.data})
-}
+  async componentDidMount() {
+    let deck = await axios.get(`${API_BASE_URL}/new/shuffle/?deck_count=1`);
+    this.setState({ deck: deck.data });
+  }
+
+  async getCard() {
+    let deck_id = this.state.deck.deck_id;
+    try {
+      let cardRes = await axios.get(
+        `https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`
+      );
+      if (!cardRes.data.success) {
+        throw new Error('No card remaining');
+      }
+      let card = cardRes.data.cards[0];
+      this.setState(st => ({
+        drawn: [
+          ...st.drawn,
+          {
+            id: card.code,
+            image: card.image,
+            name: `${card.value} of ${card.suit}`
+          }
+        ]
+      }));
+    } catch (err) {
+      alert(err);
+    }
+  }
 
   render() {
+    const cards = this.state.drawn.map(c => (
+      <Card key={c.id} name={c.name} image={c.image} />
+    ));
     return (
       <div>
         <h1>Card Dealer</h1>
+        <button onClick={this.getCard}>Get Card!</button>
+        <div className='Deck-cardarea'> {cards}</div>
       </div>
     );
   }
