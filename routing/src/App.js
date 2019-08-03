deba4950ea2731783ab82b82997ec6e342b1188b
Olivia Ester <oliviaesterdev@gmail.com>
2019-08-02 20:54:14 -0700

    practice

---
+++ routing/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import About from './About';
+import Dog from './Dog';
+import Contact from './Contact';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <Route path='/' component={About} />
+        <Route path='/dog' component={Dog} />
+        <Route path='/contact' component={Contact} />
+      </div>
+    );
+  }
+}
+
+export default App;
