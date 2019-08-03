371829c86ee1879333887696e39a727a9dd55139
Olivia Ester <oliviaesterdev@gmail.com>
2019-08-02 21:29:50 -0700

    practice

--- routing/src/App.js
+++ routing/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch, NavLink } from 'react-router-dom';
 import About from './About';
 import Dog from './Dog';
 import Contact from './Contact';
@@ -9,9 +9,22 @@ class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <Route path='/' component={About} />
-        <Route path='/dog' component={Dog} />
-        <Route path='/contact' component={Contact} />
+        <nav>
+          <NavLink exact activeClassName='active-link' to='/'>
+            About
+          </NavLink>
+          <NavLink exact activeClassName='active-link' to='/dog'>
+            Dog
+          </NavLink>
+          <NavLink exact activeClassName='active-link' to='/contact'>
+            Contact
+          </NavLink>
+        </nav>
+        <Switch>
+          <Route exact path='/' component={About} />
+          <Route exact path='/dog' component={Dog} />
+          <Route exact path='/contact' component={Contact} />
+        </Switch>
       </div>
     );
   }
