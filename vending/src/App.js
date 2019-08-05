a23cd50264610d9e28576c295eac875ca0073edc
Olivia Ester <oliviaesterdev@gmail.com>
2019-08-05 11:53:56 -0700

    practice

--- vending/src/App.js
+++ vending/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import Chips from './Chips';
 import VendingMachine from './VendingMachine';
 import Sardines from './Sardines';
@@ -9,10 +9,12 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <VendingMachine />
-      <Chips />
-      <Sardines />
-      <Soda />
+      <Switch>
+        <Route exact path='/' render={() => <VendingMachine />} />
+        <Route exact path='/chips' render={() => <Chips />} />
+        <Route exact path='/soda' render={() => <Soda />} />
+        <Route exact path='/sardines' render={() => <Sardines />} />
+      </Switch>
     </div>
   );
 }
