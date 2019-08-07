edc910e0bae7bdcc2666e5f93ed6535d79cf972b
Olivia Ester <oliviaesterdev@gmail.com>
2019-08-07 12:14:35 -0700

    practice

--- dogs/src/DogList.js
+++ dogs/src/DogList.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react';
 class DogList extends Component {
   render() {
     return (
-      <div>
+      <div className='DogList'>
         <h1 className='display-1 text-center'>Dog List!</h1>
+        <div className='container'>
+          <div className='row'>
+            {this.props.dogs.map(d => (
+              <div className='col-4' key={d.name}>
+                <img src={d.src} alt={d.name} />
+                <h3>{d.name}</h3>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
