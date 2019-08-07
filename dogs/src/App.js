2fc98b1b365e0c198db999ce533b0254d004a503
Olivia Ester <oliviaesterdev@gmail.com>
2019-08-07 13:11:26 -0700

    practice

--- dogs/src/App.js
+++ dogs/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import DogList from './DogList';
+import DogDetails from './DogDetails';
 import { Switch, Route } from 'react-router-dom';
 import whiskey from './images/whiskey.jpeg';
 import tubby from './images/tubby.jpeg';
@@ -42,8 +43,22 @@ class App extends React.Component {
     ]
   };
   render() {
+    const getDog = props => {
+      let name = props.match.params.name;
+      let currentDog = this.props.dogs.find(
+        dog => dog.name.toLowerCase() === name.toLowerCase()
+      );
+      return <DogDetails {...props} dog={currentDog} />;
+    };
     return (
-      <Route path='/dogs' render={() => <DogList dogs={this.props.dogs} />} />
+      <Switch>
+        <Route
+          exact
+          path='/dogs'
+          render={() => <DogList dogs={this.props.dogs} />}
+        />
+        <Route exact path='/dogs/:name' render={getDog} />
+      </Switch>
     );
   }
 }
