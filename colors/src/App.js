0d87d1160c1b8b3d57c57ba58b2d825f4391b823
Olivia Ester <oliviaesterdev@gmail.com>
2019-08-09 17:00:59 -0700

    practice

---
+++ colors/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Palette from './Palette';
+import seedColors from './seedColors';
+
+function App() {
+  return (
+    <div className='App'>
+      <Palette {...seedColors[2]} />
+    </div>
+  );
+}
+
+export default App;
