42360a44329f1b4642eddbe82f227713c6b496b5
Olivia Ester <oliviaesterdev@gmail.com>
2019-08-12 18:10:11 -0700

    practice

--- colors/src/App.js
+++ colors/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 
-function App() {
-  return (
-    <div className='App'>
-      <Palette {...seedColors[2]} />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    console.log(generatePalette(seedColors[4]));
+    return (
+      <div className='App'>
+        <Palette {...seedColors[2]} />
+      </div>
+    );
+  }
 }
 
 export default App;
